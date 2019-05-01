6ddf855fd363182efd6f7175ebdc4334a4bdd03f
varun <varun.vbr@gmail.com>
2019-05-01 14:57:20 +0530

    Category pages

--- app/components/anonymousCategoryPage/anonymousCategoryPageController.js
+++ app/components/anonymousCategoryPage/anonymousCategoryPageController.js
@@ -1,12 +1,21 @@
     bookBoxApp.controller("anonymousCategoryPageCtrl",     
                           ['$scope',
+                           '$location',
                            'anonymousCategoryService',
                            'userManagementService',
-                           function($scope,anonymousCategoryService,userManagementService){
+                           'anonymousBookService',
+                           function($scope,$location,anonymousCategoryService,userManagementService,anonymousBookService){
+                               $scope.searchText;
+                               $scope.selectedItem;
+                               $scope.selectedType='None';
                                $scope.categoryDetails={name:'',books:[]}; 
                                $scope.init=function(){debugger;
                                    $scope.categoryDetails=anonymousCategoryService.getCategoryDetails();
                                }
                                $scope.init();
+                               $scope.goToAnonymousBookPage=function(book){
+                                     anonymousBookService.setBookToDisplay(book);
+                                     $location.path("/bookDetails");
+                                }
          
     }])
