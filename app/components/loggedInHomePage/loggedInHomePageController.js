4281afa386660424cd0da592d11503ecd2b7618d
varun <varun.vbr@gmail.com>
2019-09-08 21:03:55 +0530

    Logged In home page almost done

--- app/components/loggedInHomePage/loggedInHomePageController.js
+++ app/components/loggedInHomePage/loggedInHomePageController.js
@@ -4,10 +4,16 @@ bookBoxApp.controller("loggedInHomePageCtrl",['$scope',
                                                  $scope.searchText;
                                                  $scope.selectedItem;
                                                  $scope.selectedType='None';
+                                                 $scope.currentReads=[];
                 
                                                  $scope.userInfo={};
-                                                 $scope.init=function(){
+                                                 $scope.init=function(){debugger;
                                                     $scope.userInfo=userManagementService.getCurrentUser();
+                                                    for(var i=0; i< $scope.userInfo.currentReads.length; i++){
+                                                        $scope.currentReads.push($scope.userInfo.currentReads[i].book)
+                                                    }                    
+                                                    $scope.booksLoaded=true;                    
+                                                     
                                                  }
                                                  $scope.init();
                                              }])
